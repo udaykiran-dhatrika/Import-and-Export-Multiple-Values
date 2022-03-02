a93f96ac2b0979eb090c9f17549703b36e0a7b63
udaykiran <udaykiran.dhatrika@gmail.com>
2022-03-02 06:29:39 +0000

    Import and Export Multiple Values solution

---
+++ exportMultipleValues.mjs
@@ -0,0 +1,10 @@
+export let myArray = ["camel", 265, true, "5.6"];
+export let bulb = {
+  watts: 10,
+  type: "LED",
+};
+
+let number = 4;
+export function multiplyByFour(num) {
+  return 4 * num;
+}
